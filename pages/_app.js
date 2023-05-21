7ae4e3b8117a0728c4010aa9eafc98e3f0c1f1e2
Yash Garg <yashygkf@gmail.com>
2023-05-21 13:08:21 +0530

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import MainLayout from "../layout/mainLayout";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<MainLayout>
 			<Component {...pageProps} />
+			<Analytics />
 		</MainLayout>
 	);
 }
