93bf2a9d6148637c6dd79ef875b3293c89e5e232
Yash Garg <yashygkf@gmail.com>
2023-05-25 01:00:28 +0530

    feat: basic layout improved

--- components/grafiti.jsx
+++ components/grafiti.jsx
@@ -1,17 +1,36 @@
 import { useSignMessage } from "wagmi";
 
 export default function Grafiti({}) {
-  const { data, error, loading, signMessage } = useSignMessage({
-    message: "gm wagmi frens",
-    onSuccess: (data) => {
-      console.log(data);
-    },
-  });
+  //! URGENT: Think of some cool messsage to sign.
+  const { data, error, signMessage, isSuccess, isLoading, isError } =
+    useSignMessage({
+      message: "gm wagmi frens",
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
 
-  // returning componeneting showcasing a button to sign the message
+  // TODO: 1. Customize the message, button, and the text that is displayed
+  // TODO: 2. Clip the hash to 5 characters and Add a button to copy the hash to the clipboard
   return (
     <div>
       <button onClick={() => signMessage()}>Sign Message</button>
+      <div>
+        {" "}
+        Your Hash{" "}
+        {isSuccess ? (
+          <div>{data}</div>
+        ) : isLoading ? (
+          <div> wait for it!!</div>
+        ) : isError ? (
+          <div>{error}</div>
+        ) : (
+          <div>You need to Sign the message first!</div>
+        )}
+      </div>
     </div>
   );
 }
